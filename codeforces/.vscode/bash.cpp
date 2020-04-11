81033600f41ed8086b04d12ba9ce221fe9a324cb
shivansh2598 <shivanshsinha2598@gmail.com>
2020-04-11 15:13:46 +0530

    segment tree code added

--- codeforces/.vscode/bash.cpp
+++
@@ -1,5 +0,0 @@
-
-int main(void)
-{
-    system("cat ../deadline.cpp");
-}
---
+++ codeforces/eugeneandanarryay.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef long double ld;
+#define fr(i, k, n) for (lli i = k; i < lli(n); ++i)
+#define pb push_back
+using namespace std;
+
+void vprint(vector<int>&v)
+{  for(int i=0;i<v.size();i++)cout<<v[i]<<' ';cout<<endl;}
+
+void aprint(int *arr,int n)
+{  for(int i=0;i<n;i++)cout<<arr[i]<<' ';cout<<endl;}
+
+
+int main()
+{
+   lli a;
+   cin>>a;
+   vector<lli>vect;
+   fr(i,0,n)
+   {
+       lli x;
+       cin>>x;
+       vect.pb(x);
+   }
+   
+}
